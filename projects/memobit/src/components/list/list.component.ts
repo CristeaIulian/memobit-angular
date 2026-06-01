0c0a47ef2e3eb1fc683bfd40ad3a935ac025233f
Iulian <iulianx@gmail.com>
2026-06-01 08:49:46 +0300

    list: added collapsible grouping support

--- projects/memobit/src/components/list/list.component.ts
+++ projects/memobit/src/components/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { ListItems } from './types';
 
 @Component({
@@ -7,6 +7,34 @@ import { ListItems } from './types';
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss',
 })
-export class ListComponent {
+export class ListComponent implements OnChanges {
   @Input() public items: ListItems[] = [];
+
+  public expanded: boolean[] = [];
+
+  public ngOnChanges(changes: SimpleChanges): void {
+    if (!changes['items']) {
+      return;
+    }
+
+    this.items.forEach((category, index) => {
+      const hasSelected = category.items.some((item) => item.isSelected);
+      const isFirst = index === 0;
+
+      if (this.expanded[index] === undefined) {
+        this.expanded[index] = isFirst || hasSelected;
+        return;
+      }
+
+      if (hasSelected) {
+        this.expanded[index] = true;
+      }
+    });
+
+    this.expanded.length = this.items.length;
+  }
+
+  public toggle(index: number): void {
+    this.expanded[index] = !this.expanded[index];
+  }
 }
--- projects/memobit/src/components/list/list.module.ts
+++ projects/memobit/src/components/list/list.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 
 import { ListComponent } from './list.component';
 import { RouterLink } from '@angular/router';
-import { NgForOf } from '@angular/common';
+import { NgForOf, NgIf } from '@angular/common';
+import { IconModule } from '../icon';
 
 @NgModule({
   declarations: [ListComponent],
   exports: [ListComponent],
-  imports: [RouterLink, NgForOf],
+  imports: [RouterLink, NgForOf, NgIf, IconModule],
 })
 export class ListModule {}
